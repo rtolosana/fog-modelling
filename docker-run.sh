af0913c21578bdaee96b4c06a21ba68ccc13a728
rtolosana <36404195+rtolosana@users.noreply.github.com>
2021-09-08 02:30:11 +0200

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -4,5 +4,3 @@ XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 docker run -it --rm --device=/dev/video0 -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -v ${PWD}:/result  renew
 xhost -local:docker
-
-

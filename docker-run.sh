fe630aa5a0d0d592708feb2277c509679ae9f7a6
rtolosana <36404195+rtolosana@users.noreply.github.com>
2021-11-10 02:25:17 +0100

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -2,5 +2,5 @@ xhost +local:docker
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-docker run -it --rm -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -v ${PWD}:/result fog_modelling
+docker run -it --rm -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -v ${PWD}:/results fog_modelling
 xhost -local:docker
